43fdf06d17e904484bcba23aa029d63bd572e204
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2018-11-29 20:26:22 -0200

    Hooking with lifecycle methods

--- countdownchamp/src/App.jsx
+++ countdownchamp/src/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      deadline: 'December 25, 2017',
+      deadline: 'December 25, 2018',
       newDeadLine: ''
     };
   }
@@ -22,7 +22,7 @@ class App extends Component {
       <div className="App">
         <div className="App-title">Countdown to {this.state.deadline}</div>
         <Clock
-          deadline={this.state.newDeadLine}
+          deadline={this.state.deadline}
         />
         <div>
           <input onChange={event => this.setState({ newDeadLine: event.target.value })} placeholder='new date'></input>
