e6e092090b3913a087bed8e9563abcc3f118b117
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2018-12-11 21:40:38 -0200

    Navigating with react-router

--- goalcoach/src/index.js
+++ goalcoach/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Route path="/app" exact component={App} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
+    </div>
+  </Router>, document.getElementById('root')
 )
+
