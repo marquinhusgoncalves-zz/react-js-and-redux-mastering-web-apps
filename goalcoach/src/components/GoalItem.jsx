9ed1b8ea8a30539359928aa082a539d0969367f3
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2018-12-17 23:58:55 -0200

    Completing a goal

--- goalcoach/src/components/GoalItem.jsx
+++ goalcoach/src/components/GoalItem.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { completeGoalRef } from '../firebase';
 
 class GoalItem extends Component {
+  completeGoal() {
+    const { email } = this.props.user;
+    const { title } = this.props.goal;
+    console.log(email, title);
+    completeGoalRef.push({ email, title });
+  }
+
   render() {
     const { email, title } = this.props.goal;
     return (
       <div style={{ margin: '5px' }}>
         <strong>{email}</strong>
         <span> submitted by <em>{title}</em></span>
+        <button
+          className='btn btn-sm btn-primary'
+          onClick={() => this.completeGoal()}
+        >
+          Complete
+        </button>
       </div>
     )
   }
 }
 
-export default GoalItem;
+function mapStateToProps(state) {
+  const { user } = state;
+  return {
+    user
+  }
+}
+
+export default connect(mapStateToProps, null)(GoalItem);
